174bd01d5dfd97a53f270f90edd0751c3f957dab
Evseev-Oleg <74254560+Evseev-Oleg@users.noreply.github.com>
2021-05-01 21:04:22 +0400

    Evseev add curs oop

---
+++ Task/Task/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace Task
+{
+    class Product
+    {
+        public string Title { get; set; }
+        public double CostPerKg { get; set; }
+        public double QuantityKg { get; set; }        
+
+        public Product(string title, double costPerKg, double quantityKg)
+        {
+            Title = title;
+            CostPerKg = costPerKg;
+            QuantityKg = quantityKg;            
+        }
+
+        public Product()
+        {
+
+        }
+    }
+}
